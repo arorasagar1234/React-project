53ffd19cd431ebf50509b0c3f8eea5c1ecacb6f5
sagar arora <sagararora791@gmail.com>
2022-10-16 17:19:15 +0530

    Initial React Project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './Header.js'
+
+// This is JSX - javascript XML (flavour of javascript looking like html)
+function App() {
+  return (
+    <Header/>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+function Header()   {
+    return(
+        <nav>
+            <h1>Sagar Restro</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Header;
